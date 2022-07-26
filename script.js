5237d1eb3f529e9fb82416c539d6469cb3739368
Murillo Pinheiro de Oliveria <murillop.o06@gmail.com>
2022-07-26 15:31:04 -0300

    update js

--- script.js
+++ script.js
@@ -4,12 +4,15 @@ const adviceText = advice.querySelector(".advice-text")
 
 const refreshBtn = document.querySelector("#refresh-btn")
 
-refreshBtn.addEventListener("click", function() {
+window.addEventListener("load", () => getMessage())
+
+refreshBtn.addEventListener("click", () => getMessage())
+
+function getMessage() {
     fetch("https://api.adviceslip.com/advice")
     .then((resp) => resp.json())
     .then(data => {
         adviceID.innerText = `Advice # ${data.slip.id}`
         adviceText.innerText = `"${data.slip.advice}"`
-
     }) 
-})
+}
